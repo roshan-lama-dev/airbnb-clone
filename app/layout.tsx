88b85b84ca83ce97dacb240aa2544f7daa07948f
Havochalo <officialmail85@gmail.com>
2023-07-04 23:05:43 +1000

    Navbar completed

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { Nunito } from "next/font/google";
+import Navbar from "./components/navbar/Navbar";
+import Modal from "./components/modals/Modal";
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Airbnb",
+  description: "Airbub Clone",
+};
+
+const font = Nunito({
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Modal isOpen />
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
